00d34678e6c36e21d03912e947b5187d26e1d79d
kevin <kevin.zp.chang@gmail.com>
2017-05-24 21:44:36 -0400

    Initial commit

---
+++ src/start_menu.java
@@ -0,0 +1,21 @@
+
+import processing.core.PApplet;
+
+public class start_menu extends PApplet {
+    public static void main(String[] args) {
+        PApplet.main("start_menu", args);
+    }
+
+    public void settings() {
+
+
+    }
+
+    public void draw() {
+
+    }
+
+    public void setup() {
+
+    }
+}
