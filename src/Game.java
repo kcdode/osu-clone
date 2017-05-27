67e09df427f8808cfb38f8cf345e88050ba3fdb0
kevin <kevin.zp.chang@gmail.com>
2017-05-27 00:40:56 -0400

    start menu

---
+++ src/Game.java
@@ -0,0 +1,26 @@
+// Created on 5/26/2017
+
+import processing.core.PApplet;
+
+public class Game extends PApplet {
+
+//    public static void main(String[] args) {
+//        PApplet.main("Game", args);
+//    }
+
+    static void run() {
+        PApplet.main("Game", null);
+    }
+
+    public void settings() {
+        size(1920, 1080);
+    }
+
+    public void setup() {
+
+    }
+
+    public void draw() {
+
+    }
+}
--- src/StartMenu.java
+++ src/StartMenu.java
@@ -8,7 +8,11 @@ public class StartMenu extends PApplet {
 
     private MenuButton play;
     private MenuButton load;
-    private int v = 1;
+    private String filepath;
+    private boolean updateString; // toggles with pressing load
+    private AudioPlayer audio;
+    private int f; // only open one new frame
+
 
     public static void main(String[] args) {
         PApplet.main("StartMenu", args);
@@ -16,17 +20,23 @@ public class StartMenu extends PApplet {
 
     public void settings() {
         size(1920, 1080);
+        f = 0;
     }
 
     public void draw() {
+        int v = 1;
         background(255, 255, 240);
 
-
         if (mousePressed) {
-            if (play.isClicked()) {
-                System.out.println("Play");
+            if (play.isClicked() && f == 0) {
+                f++;
+                Game.run();
+                audio.close();
+                surface.setVisible(false);
             } else if (load.isClicked()) {
-                System.out.println("load");
+                updateString = true;
+            } else {
+                updateString = false;
             }
         }
 
@@ -46,25 +56,39 @@ public class StartMenu extends PApplet {
         play.display();
         fill(0 , 191, 255);
         load.display();
+        fill(0, 0, 0);
+        //textAlign(CENTER);
+        text(filepath, 810, 925);
 
     }
 
     public void setup() {
+        filepath = "Filepath";
         colorMode(RGB, 255, 255, 255);
         noStroke();
         background(255, 255, 255);
 
+        // Initialize menu buttons
         surface.setTitle("OSU! 2: Electric Boogaloo");
         play = new MenuButton("PLAY", 810, 700, this);
         load = new MenuButton("LOAD", 810, 800, this);
 
-
-
-
         // Menu song (beginning of Sweet Dreams by Eurythmics)
-        AudioPlayer audio = new Minim(this).loadFile("music/sweetdreams.mp3");
+        audio = new Minim(this).loadFile("music/sweetdreams.mp3");
         audio.play();
         audio.loop();
+    }
+
+    public void keyPressed() {
+        if (!updateString) return;
+
+        if (keyCode == BACKSPACE) {
+            filepath = "";
+        } else if (keyCode == ENTER) {
+            updateString = false;
+        } else {
+            filepath += key;
+        }
     }
 
 }
